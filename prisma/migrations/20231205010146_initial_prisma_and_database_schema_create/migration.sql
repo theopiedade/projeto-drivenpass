029e5af4b103d20a8b7f76380b083570eeff5d54
Theo <theo.piedade@gmail.com>
2023-12-04 22:15:54 -0300

    User schema modifyied

---
+++ prisma/migrations/20231205010146_initial_prisma_and_database_schema_create/migration.sql
@@ -0,0 +1,119 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Address` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Booking` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Enrollment` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Event` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Hotel` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Payment` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Room` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Session` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Ticket` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `TicketType` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Address" DROP CONSTRAINT "Address_enrollmentId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Booking" DROP CONSTRAINT "Booking_roomId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Booking" DROP CONSTRAINT "Booking_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Enrollment" DROP CONSTRAINT "Enrollment_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Payment" DROP CONSTRAINT "Payment_ticketId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Room" DROP CONSTRAINT "Room_hotelId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Session" DROP CONSTRAINT "Session_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Ticket" DROP CONSTRAINT "Ticket_enrollmentId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Ticket" DROP CONSTRAINT "Ticket_ticketTypeId_fkey";
+
+-- DropTable
+DROP TABLE "Address";
+
+-- DropTable
+DROP TABLE "Booking";
+
+-- DropTable
+DROP TABLE "Enrollment";
+
+-- DropTable
+DROP TABLE "Event";
+
+-- DropTable
+DROP TABLE "Hotel";
+
+-- DropTable
+DROP TABLE "Payment";
+
+-- DropTable
+DROP TABLE "Room";
+
+-- DropTable
+DROP TABLE "Session";
+
+-- DropTable
+DROP TABLE "Ticket";
+
+-- DropTable
+DROP TABLE "TicketType";
+
+-- DropTable
+DROP TABLE "User";
+
+-- DropEnum
+DROP TYPE "TicketStatus";
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "credentials" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "credentials_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "networks" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "network" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "networks_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "credentials" ADD CONSTRAINT "credentials_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "networks" ADD CONSTRAINT "networks_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
